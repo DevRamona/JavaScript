be062dfe926dda46640a689a30c4a1e818fb48b6
DevRamona <r.ingabire@alustudent.com>
2024-02-28 08:48:51 +0200

    For Day 2

---
+++ Day 2/variables.js
@@ -0,0 +1,13 @@
+let greeting = "Hello, World!";
+console.log(greeting);
+
+let age = 19;
+console.log(age);
+
+let number = 42;
+console.log(number);
+number = 100;
+console.log(number);
+
+const pi = 3.14;
+console.log(pi);
