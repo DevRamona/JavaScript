0c4e1b2a320b6fffbc4fd789c1062bd5cef3aab7
DevRamona <r.ingabire@alustudent.com>
2024-02-28 09:12:37 +0200

    For Day 2

--- Day 2/variables.js
+++ Day 2/variables.js
@@ -1,13 +1,13 @@
+// Variable declaration
 let greeting = "Hello, World!";
-console.log(greeting);
+console.log(greeting); 
 
-let age = 19;
-console.log(age);
-
+// Variable reassignment
 let number = 42;
 console.log(number);
 number = 100;
 console.log(number);
 
+// Constants
 const pi = 3.14;
 console.log(pi);
