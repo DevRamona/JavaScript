980beab0ba79eb24040e7650c433b7f8e112a6cd
DevRamona <r.ingabire@alustudent.com>
2024-02-29 14:27:05 +0200

    notes for Day 5

---
+++ Day5/mathfunctions.js
@@ -0,0 +1,35 @@
+let num = -10;
+console.log(Math.abs(num));
+
+var number = 3.8375634356;
+//Rounds a number to the nearest integer
+console.log(Math.ceil(number));
+//Rounds a number down
+console.log(Math.floor(number));
+
+//Returns a number that is between 0-1//
+console.log(Math.random(number));
+
+//Rounds a number down or up to the nearest integer depending on the decimal number 
+console.log(Math.round(number));
+
+
+var num1 = 5.2;
+console.log(Math.floor(num1));
+console.log(Math.round(num1));
+console.log(Math.random(num1));
+console.log(Math.ceil(num1));
+
+let maxNumber = Math.max(10,5,20,25,15);
+console.log(maxNumber);
+
+let minNumber = Math.min(10,-5,20,25,15);
+console.log(minNumber);
+
+let x = 64;
+let y = 4;
+let result = Math.pow(x,y);
+console.log(result);
+
+let num2 = 36;
+console.log(Math.sqrt(num2));
